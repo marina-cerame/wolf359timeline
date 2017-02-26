d9d3dd087249df71f293f8031468cd44b3e0d5fd
Marina Cerame <marina.cerame@gmail.com>
2017-02-26 11:09:24 -0600

    setup server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,17 @@
-'use strict'
+'use strict';
+
+require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
