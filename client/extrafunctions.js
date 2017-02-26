15a9f3025207673cc69d0d572ec88aab2f84a079
Marina Cerame <marina.cerame@gmail.com>
2017-02-26 17:17:33 -0600

    imported styling

---
+++ client/extrafunctions.js
@@ -0,0 +1,7 @@
+/* global $ */
+
+// Hide unnecessary elements on title card
+$('#wolf-359-marker').css('display', 'none');
+$('#wolf-359').find('h3').css('display', 'none');
+
+$('p').css('color', 'black');
--- client/newtimeline.js
+++ client/newtimeline.js
@@ -1,4 +1,4 @@
-/* global episodes, season1, season2, season3, $ */
+/* global episodes, season1, season2, season3 */
 
 const timelineObject = {
   events: episodes,
@@ -10,6 +10,3 @@ const timelineObject = {
 };
 
 const timeline = new TL.Timeline('timeline-embed', timelineObject, { scale_factor: 5 });
-
-$('#wolf-359-marker').css('display', 'none');
-$('#wolf-359').find('h3').css('display', 'none');
