52572274ad2f22083065ae2a6e6aa53a78e352ab
Marina Cerame <marina.cerame@gmail.com>
2017-02-26 11:15:32 -0600

    reformatting for heroku

--- server/server.js
+++ server/server.js
@@ -4,13 +4,14 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static('client'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
