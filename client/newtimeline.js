9a3cb0a1b1d655feb2426d41f7fb391e52a24fe8
Marina Cerame <marina.cerame@gmail.com>
2017-02-26 14:14:47 -0600

    add season 3 file

--- client/newtimeline.js
+++ client/newtimeline.js
@@ -1,10 +1,4 @@
-/* global episodes, season1, season2 $ */
-
-const season3 = {
-  start_date: { year: 2015, month: 3, day: 21 },
-  end_date: { year: 2016, month: 5, day: 11 },
-  text: { headline: 'Season 3' },
-};
+/* global episodes, season1, season2, season3, $ */
 
 const timelineObject = {
   events: episodes,
---
+++ client/season3.js
@@ -0,0 +1,5 @@
+const season3 = {
+  start_date: { year: 2015, month: 3, day: 21 },
+  end_date: { year: 2016, month: 5, day: 11 },
+  text: { headline: 'Season 3' },
+};
