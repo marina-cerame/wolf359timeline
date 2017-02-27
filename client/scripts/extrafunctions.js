db1ceba48c2884bff431215ee11a46b9d1db6b12
Marina Cerame <marina.cerame@gmail.com>
2017-02-27 17:21:52 -0600

    season 1 mission days display correctly

--- client/scripts/extrafunctions.js
+++ client/scripts/extrafunctions.js
@@ -17,7 +17,7 @@ $(document).ready(() => {
   // Play Initial Audio
   const $audio = $('audio')[0];
   $audio.play();
-  $audio.volume = 0.7;
+  $audio.volume = 0.8;
 
   // Play/Mute Audio Control
   let audioStatus = true;
@@ -41,26 +41,50 @@ $(document).ready(() => {
     if (audioStatus) {
       $audio.pause();
       const title = $('.tl-timemarker-active').attr('id');
-      console.log(title);
+      // console.log(title);
     }
   });
 
 // ========== DISPLAY DATE/MISSION DAY ========== //
+// necessary variables and helpers
   let displayDate;
-  let reset = true;
+  let shouldSave = true;
 
-  $('body').on('slide-change', () => { reset = true; });
+  const hephMission = 'Hephaestus Mission Day';
+  const missionDays = {
+    'succulent-rat-killing-tar': `${hephMission} 448`,
+    'little-revolucion': `${hephMission} 455`,
+    'discomforts-pains-and-irregularities': `${hephMission} 482`,
+    'cataracts-and-hurricanoes': '* No Mission Day Given',
+    'cigarette-candy': `${hephMission}s 490 to 494`,
+    'super-energy-saver-mode': `${hephMission}s 500 to 501`,
+    'the-sound-and-the-fury': `${hephMission} 512`,
+    'box-953': `${hephMission} 525`,
+    'the-empty-man-cometh': `${hephMission} 530`,
+    'extreme-danger-bug': '* No Mission Day Given',
+    'am-i-alone-now': '* No Mission Day Given',
+    'deep-breaths': `${hephMission} 580`,
+    'gas-me-twice': `${hephMission} 580`,
+  };
+
+  $('body').on('slide-change', () => { shouldSave = true; });
 
   const headlineHoverIn = (e) => {
-    // console.log($(e.target).parents('.tl-slide').attr('id'));
-    if (reset) {
+    console.log($(e.target).parents('.tl-slide').attr('id'));
+    // get episode
+    const title = $(e.target).parents('.tl-slide').attr('id');
+
+    // only save the target text if this is the first time
+    if (shouldSave) {
       displayDate = $(e.target).text();
-      reset = false;
+      shouldSave = false;
     }
+
+    // switch to mission day
     $(e.target)
       .stop()
       .fadeOut(250, () => {
-        $(e.target).text('new text!');
+        $(e.target).text(missionDays[title]);
       })
       .fadeIn(250);
   };
