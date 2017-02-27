c9d4aea099f21baafb28f05a92f579e3de2e06ea
Marina Cerame <marina.cerame@gmail.com>
2017-02-27 15:41:49 -0600

    begin season 2 text

--- client/scripts/season2.js
+++ client/scripts/season2.js
@@ -6,138 +6,183 @@ const season2 = {
   text: { headline: 'Season 2' },
 };
 
+let kumbayaText = '<small>Written by Gabriel Urbina</small> <br>';
+kumbayaText += '';
+
 const kumbaya = {
   start_date: { year: 2014, month: 12, day: 26 },
   text: {
     headline: 'The Kumbaya Approach',
-    text: 'test test test',
+    text: kumbayaText,
   },
 };
 
-const whatsup = {
+let whatsUpText = '<small>Written by Gabriel Urbina</small> <br>';
+whatsUpText += '';
+
+const whatsUp = {
   start_date: { year: 2014, month: 12, day: 28 },
   text: {
     headline: 'What\'s Up, Doc?',
-    text: 'test test test',
+    text: whatsUpText,
   },
 };
+
+let painfullyText = '<small>Written by Gabriel Urbina</small> <br>';
+painfullyText += '';
 
 const painfully = {
   start_date: { year: 2015, month: 1, day: 11 },
   text: {
     headline: 'Painfully Ever After',
-    text: 'test test test',
+    text: painfullyText,
   },
 };
+
+let bachText = '<small>Written by Gabriel Urbina</small> <br>';
+bachText += '';
 
 const bach = {
   start_date: { year: 2015, month: 1, day: 21 },
   text: {
     headline: 'Bach to the Future',
-    text: 'test test test',
+    text: bachText,
   },
 };
+
+let assistanceText = '<small>Written by Gabriel Urbina</small> <br>';
+assistanceText += '';
 
 const assistance = {
   start_date: { year: 2015, month: 2, day: 2 },
   text: {
     headline: 'Happy to Be of Assistance',
-    text: 'test test test',
+    text: assistanceText,
   },
 };
 
-const killhilbert = {
+let killHilbertText = '<small>Written by Sarah Shachat</small> <br>';
+killHilbertText += '';
+
+const killHilbert = {
   start_date: { year: 2015, month: 2, day: 2 },
   text: {
     headline: 'Let\'s Kill Hilbert',
-    text: 'test test test',
+    text: killHilbertText,
   },
 };
+
+let paranoiaText = '<small>Written by Gabriel Urbina</small> <br>';
+paranoiaText += '';
 
 const paranoia = {
   start_date: { year: 2015, month: 2, day: 8 },
   text: {
     headline: 'The Paranoia Game',
-    text: 'test test test',
+    text: paranoiaText,
   },
 };
+
+let commandingText = '<small>Written by Sarah Shachat</small> <br>';
+commandingText += '';
 
 const commanding = {
   start_date: { year: 2015, month: 2, day: 14 },
   end_date: { year: 2015, month: 2, day: 28 },
   text: {
     headline: 'Minkowski Commanding',
-    text: 'test test test',
+    text: commandingText,
   },
 };
+
+let destructionText = '<small>Written by Gabriel Urbina</small> <br>';
+destructionText += '';
 
 const destruction = {
   start_date: { year: 2015, month: 2, day: 28 },
   text: {
     headline: 'Mutually Assured Destruction',
-    text: 'test test test',
+    text: destructionText,
   },
 };
+
+let noPressureText = '<small>Written by Sarah Shachat</small> <br>';
+noPressureText += '';
 
 const noPressure = {
   start_date: { year: 2015, month: 3, day: 10 },
   text: {
     headline: 'No Pressure',
-    text: 'test test test',
+    text: noPressureText,
   },
 };
+
+let brainDamageText = '<small>Written by Gabriel Urbina</small> <br>';
+brainDamageText += '';
 
 const brainDamage = {
   start_date: { year: 2015, month: 3, day: 12 },
   text: {
     headline: 'Tactical Brain Damage',
-    text: 'test test test',
+    text: brainDamageText,
   },
 };
+
+let superheroText = '<small>Written by Sarah Shachat</small> <br>';
+superheroText += '';
 
 const superhero = {
   start_date: { year: 2015, month: 3, day: 14 },
   text: {
     headline: 'Lame-o Superhero Origin Story',
-    text: 'test test test',
+    text: superheroText,
   },
 };
+
+let noHarmText = '<small>Written by Zach Valenti</small> <br>';
+noHarmText += '';
 
 const noHarm = {
   start_date: { year: 2015, month: 3, day: 14 },
   end_date: { year: 2015, month: 3, day: 21 },
   text: {
     headline: 'Do No Harm',
-    text: 'test test test',
+    text: noHarmText,
   },
   display_date: 'March 14, 2015 – March 21, 2015',
 };
+
+let knockKnockText = '<small>Written by Gabriel Urbina</small> <br>';
+knockKnockText += '';
 
 const knockKnock = {
   start_date: { year: 2015, month: 3, day: 21, hour: 16 },
   text: {
     headline: 'Knock Knock',
-    text: 'test test test',
+    text: knockKnockText,
   },
   display_date: 'March 21, 2015',
 };
+
+let whosThereText = '<small>Written by Gabriel Urbina</small> <br>';
+whosThereText += '';
 
 const whosThere = {
   start_date: { year: 2015, month: 3, day: 21, hour: 17 },
   text: {
     headline: 'Who\'s There?',
-    text: 'test test test',
+    text: whosThereText,
   },
   display_date: 'March 21, 2015',
 };
 
 episodes = episodes.concat([
   kumbaya,
-  whatsup,
+  whatsUp,
   painfully,
   bach,
   assistance,
-  killhilbert,
+  killHilbert,
   paranoia,
   commanding,
   destruction,
