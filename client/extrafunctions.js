24128745903aa8acff13839042eb131a2c55b89c
Marina Cerame <marina.cerame@gmail.com>
2017-02-26 19:59:18 -0600

    might have background scaling correctly...

--- client/extrafunctions.js
+++ client/extrafunctions.js
@@ -8,12 +8,13 @@ $('#wolf-359').find('h3').css('display', 'none');
 $('p').css('color', 'black');
 
 // change background on slide change
-$('.tl-slidenav-icon, .tl-timemarker').click(() => {
-  const currImage = $('body').css('background-image');
-  const currNumber = Number(currImage[currImage.length - 7]);
-  let bgNumber = Math.floor(Math.random() * 9) + 1;
-  while (currNumber === bgNumber) {
-    bgNumber = Math.floor(Math.random() * 9) + 1;
-  }
-  $('body').css('background-image', `url("./backgrounds/bg${bgNumber}.png")`);
-});
+$('.tl-slidenav-icon, .tl-slidenav-title, .tl-slidenav-description, .tl-timemarker')
+  .click(() => {
+    const currImage = $('body').css('background-image');
+    const currNumber = Number(currImage[currImage.length - 7]);
+    let bgNumber = Math.floor(Math.random() * 9) + 1;
+    while (currNumber === bgNumber) {
+      bgNumber = Math.floor(Math.random() * 9) + 1;
+    }
+    $('body').css('background-image', `url("./backgrounds/bg${bgNumber}.png")`);
+  });
