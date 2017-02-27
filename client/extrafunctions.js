2216e1b5c42ff64d1483f87a170d59896a13dc8b
Marina Cerame <marina.cerame@gmail.com>
2017-02-26 19:14:02 -0600

    working on styling

--- client/extrafunctions.js
+++ client/extrafunctions.js
@@ -5,3 +5,8 @@ $('#wolf-359-marker').css('display', 'none');
 $('#wolf-359').find('h3').css('display', 'none');
 
 $('p').css('color', 'black');
+
+$('.tl-slidenav-icon').click(() => {
+  const color = $('body').css('background-color') === 'red' ? 'black' : 'red';
+  $('body').css('background-color', color);
+});
