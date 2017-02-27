f361ea8024d2f5440e9474bba02ff83c24f26b8a
Marina Cerame <marina.cerame@gmail.com>
2017-02-26 21:17:17 -0600

    audio visibly toggles

--- client/extrafunctions.js
+++ client/extrafunctions.js
@@ -11,3 +11,11 @@ $('.tl-slidenav-content-container, .tl-slidenav-content, .tl-timemarker')
     const bgNumber = Math.floor(Math.random() * 9) + 1;
     $('body').css('background-image', `url("./backgrounds/bg${bgNumber}.png")`);
   });
+
+// toggle audio
+let audioStatus = true;
+$('.audio-status, .tl-icon-music').click(() => {
+  audioStatus = !audioStatus;
+  $('.audio-status').text(audioStatus ? 'Audio On' : 'Audio Off');
+  $('.tl-icon-music').css('color', audioStatus ? 'rgba(225,225,225,0.8)' : 'rgba(0,0,0,0.5)');
+});
