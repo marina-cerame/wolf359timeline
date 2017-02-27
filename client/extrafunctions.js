e8ce232890b44a72ed8f8abb8e78a00a7018e678
Marina Cerame <marina.cerame@gmail.com>
2017-02-26 19:25:51 -0600

    background changes on click

--- client/extrafunctions.js
+++ client/extrafunctions.js
@@ -1,4 +1,5 @@
 /* global $ */
+console.log('loading extra functions');
 
 // Hide unnecessary elements on title card
 $('#wolf-359-marker').css('display', 'none');
@@ -6,7 +7,13 @@ $('#wolf-359').find('h3').css('display', 'none');
 
 $('p').css('color', 'black');
 
+// change background on slide change
 $('.tl-slidenav-icon').click(() => {
-  const color = $('body').css('background-color') === 'red' ? 'black' : 'red';
-  $('body').css('background-color', color);
+  const currImage = $('body').css('background-image');
+  const currNumber = Number(currImage[currImage.length - 7]);
+  let bgNumber = Math.floor(Math.random() * 9) + 1;
+  while (currNumber === bgNumber) {
+    bgNumber = Math.floor(Math.random() * 9) + 1;
+  }
+  $('body').css('background-image', `url("./backgrounds/bg${bgNumber}.png")`);
 });
